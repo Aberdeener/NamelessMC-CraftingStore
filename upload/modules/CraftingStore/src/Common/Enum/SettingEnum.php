eebef352e52c67721b1ddf9c1434d7d99928ab97
Tadhg Boyle <tadhgsmboyle@gmail.com>
2020-08-28 17:16:05 -0700

    Update SettingEnum.php

--- upload/modules/CraftingStore/src/Common/Enum/SettingEnum.php
+++ upload/modules/CraftingStore/src/Common/Enum/SettingEnum.php
@@ -3,7 +3,7 @@
 class SettingEnum
 {
     public const SERVER_NAME = 'server_name';
-    public const SERVER_TOKEN = 'server_key';
+    public const SERVER_TOKEN = 'server_token';
     public const ALLOW_GUEST = 'allow_guests';
     public const STORE_CONTENT = 'store_content';
     public const STORE_PATH = 'store_path';
